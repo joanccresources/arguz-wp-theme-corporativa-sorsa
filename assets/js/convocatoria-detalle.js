3890fbc2ecb4dde6692580dad61940fba3d85db8
Joan C <joan0492@gmail.com>
2024-08-22 17:47:13 -0500

    traducciones formularios

--- assets/js/convocatoria-detalle.js
+++ assets/js/convocatoria-detalle.js
@@ -13,6 +13,60 @@ const setInputYourSubject = () => {
   $inputPage.setAttribute("value", location.href);
 };
 
+const translateTextFormSubmit = () => {
+  const changeText = ($form) => {
+    //
+    const txtErrorFields = Array.from(
+      $form.querySelectorAll(".wpcf7-not-valid-tip")
+    );
+    txtErrorFields.forEach((txt) => {
+      txt.textContent = "Por favor, complete este campo.";
+    });
+
+    //
+    const responseError = document.querySelector(
+      ".wpcf7-form.invalid .wpcf7-response-output"
+    );
+    if (responseError) {
+      responseError.textContent =
+        "Hay un error en uno o más campos. Por favor, compruébelo y vuelva a intentarlo.";
+    }
+
+    //
+    const responseSent = document.querySelector(
+      ".wpcf7-form.sent .wpcf7-response-output"
+    );
+    if (responseSent) {
+      responseSent.textContent = "Gracias por tu mensaje. Ha sido enviado.";
+    }
+
+    setTimeout(() => {
+      //
+      const responseSent = document.querySelector(
+        ".wpcf7-form.sent .wpcf7-response-output"
+      );
+      if (responseSent) {
+        responseSent.textContent = "Gracias por tu mensaje. Ha sido enviado.";
+      }
+    }, 1200);
+  };
+
+  const $form = document.querySelector("#postula-ahora form.wpcf7-form");
+  if (!$form) return;
+  $form.addEventListener("submit", (event) => {
+    setTimeout(() => {
+      changeText($form);
+    }, 1000);
+  });
+
+  // $form.addEventListener("submit", (event) => {
+  //   setTimeout(() => {
+  //     changeText($form);
+  //   }, 1000);
+  // });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
   setInputYourSubject();
+  translateTextFormSubmit();
 });
--- functions.php
+++ functions.php
@@ -139,3 +139,115 @@ function wpc_elementor_shortcode_responsabilidades($atts)
   echo $responsabilidades;
 }
 add_shortcode("convocatoria_responsabilidades", "wpc_elementor_shortcode_responsabilidades");
+
+
+
+// test-convocatorias
+function wpc_elementor_shortcode_trabaja_convocatorias($atts)
+{
+  // Inicializa la variable de salida
+  $output = '';
+
+  // Argumentos para la consulta
+  $args = array(
+    'post_type' => 'convocatoria', // Nombre de tu post type
+    'posts_per_page' => -1,        // -1 para obtener todos los posts
+  );
+
+  // La consulta
+  $query = new WP_Query($args);
+
+  // Verifica si hay posts
+  if ($query->have_posts()) :
+    $output .= '<div
+  class="elementor-element elementor-element-61c273e convocatorias-disponibles elementor-widget elementor-widget-artech-careers"
+  data-id="61c273e"
+  data-element_type="widget"
+  id="test-convocatorias"
+  data-settings=\'{"tc_dark_mode_responsive_hide_in_dark":"no","tc_dark_mode_responsive_hide_in_light":"no"}\'
+  data-widget_type="artech-careers.default">';
+    $output .= '<div class="elementor-widget-container">';
+    $output .= '<div class="artech-careers">';
+    $count = 0;
+    while ($query->have_posts()) : $query->the_post();
+      $count++;
+      // $terms = get_the_terms(get_the_ID(), 'tipo-de-convocatoria');
+      $output .= '<a href="' . get_permalink() . '" class="career-card">';
+      $output .= '<div class="row">';
+
+      $output .= '<div class="col-lg-2 col-6">';
+      if ($count <= 9):
+        $output .= '<span class="num"> 0' . $count . '</span>';
+      else:
+        $output .= '<span class="num"> ' . $count . '</span>';
+      endif;
+      $output .= '</div>';
+
+      $terms = get_the_terms(get_the_ID(), 'tipo-de-convocatoria');
+      $output .= '<div class="col-lg-2 col-6">';
+      if ($terms && !is_wp_error($terms)) :
+        $output .= '<span class="tag"> ' . $terms[0]->name . ' </span>';
+      endif;
+      $output .= '</div>';
+
+      $output .= '<div class="col-lg-4">';
+      $output .= '<div class="inf my-4 my-lg-0">';
+      $output .= '<h6 class="title mb-2">' . get_the_title() . '</h6>';
+
+      $vigencia = get_field('vigencia');
+      if ($vigencia):
+        $output .= '<p class="description">Vigente hasta ' . $vigencia . '</p>';
+      else:
+        $output .= '<p class="description">Vigente hasta ---</p>';
+      endif;
+
+      $output .= '</div>';
+      $output .= '</div>';
+
+      $output .= '<div class="col-lg-2 col-6">';
+      $modalidad = get_field('modalidad');
+      if ($modalidad):
+        $output .= '<div class="job text-lg-center">' . $modalidad . '</div>';
+      else:
+        $output .= '<div class="job text-lg-center"></div>';
+      endif;
+      $output .= '</div>';
+
+      $output .= '<div class="col-lg-2 col-6">';
+      $output .= '<div class="artech-button fade-border-effect">';
+      $output .= '<span class="artech-button-content-wrapper">';
+      $output .= '<span class="artech-button-text">Ver<br>convocatoria</span>';
+      $output .= '<img decoding="async" src="https://corporativa.sorsa.pe/wp-content/uploads/2024/02/arrow_wh.svg&quot;" alt="Arrow" class="image">';
+      $output .= '</span>';
+      $output .= '</div>';
+      // $output .= '<div class="arrow text-lg-end">';
+      // $output .= '<img decoding="async" src="https://artech.themescamp.com/inner-pages/wp-content/uploads/sites/11/2024/03/arrow.svg" alt="" />';
+      // $output .= '</div>';
+      $output .= '</div>';
+
+      // $output .= '';
+      // $output .= '<div class="convocatoria-item">';
+      // $output .= '<h2>' . get_the_title() . '</h2>';
+
+      // Ejemplo de cómo mostrar un campo personalizado de ACF
+      // $campo_personalizado = get_field('nombre_del_campo');
+      // if ($campo_personalizado) :
+      //   $output .= '<p>' . esc_html($campo_personalizado) . '</p>';
+      // endif;
+      $output .= '</div>';
+      $output .= '</a>';
+    endwhile;
+    $output .= '</div>';
+    $output .= '</div>';
+    $output .= '</div>';
+
+    // Restablece la consulta global post data
+    wp_reset_postdata();
+  else :
+    $output .= '<p>No se encontraron convocatorias.</p>';
+  endif;
+
+  // Retorna el contenido generado para ser mostrado en el shortcode
+  return $output;
+}
+add_shortcode("trabaja_con_nosotros_convocatorias", "wpc_elementor_shortcode_trabaja_convocatorias");
